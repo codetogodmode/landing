0721843144c5b713faa62f4326607912221ab9b8
Martin Skořupa <martin.skorupa.personal@outlook.com>
2026-03-30 03:30:35 +0200

    Update Discord invite to permanent link

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ const STEPS: Step[] = [
     action: "Vytvořit účet",
     actionUrl: "https://discord.com/register",
     secondAction: "Připojit se na server",
-    secondUrl: "https://discord.gg/JYFMuUcyb",
+    secondUrl: "https://discord.gg/pMjn3CQFvu",
     icon: <DiscordIcon />,
   },
   {
